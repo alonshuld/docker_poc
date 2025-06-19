6d265ebba4db0cffb2cf421f4e5816f8cf13cbf3
alonshuld <alonshuld@gmail.com>
2025-06-19 15:42:15 +0300

    started working on image class

---
+++ image.py
@@ -0,0 +1,13 @@
+"""
+Purpose: Image class that will contain all needed information
+Author: Hanich 10
+"""
+
+
+from pydantic import BaseModel
+
+
+class Image(BaseModel):
+    """
+    Holds all information that image needs
+    """
