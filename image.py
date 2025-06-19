5d42a288f03db6df3970de393d1ca3a4841274ec
alonshuld <alonshuld@gmail.com>
2025-06-19 16:02:17 +0300

    Added image class

--- image.py
+++ image.py
@@ -4,10 +4,19 @@ Author: Hanich 10
 """
 
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
+from datetime import datetime
+from typing import List, BinaryIO
+from dockfile import Dockerfile
 
 
 class Image(BaseModel):
     """
     Holds all information that image needs
     """
+    name: str
+    id: UUID = Field(default_factory=uuid4())
+    creation_date: datetime = Field(default_factory=datetime.today())
+    files: List[BinaryIO]
+    dockerfile: Dockerfile
