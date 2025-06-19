c23c82e4f6e10ef77496bad0b994bd12a5b24d5c
alonshuld <alonshuld@gmail.com>
2025-06-19 11:32:17 +0300

    Added instruction class

---
+++ instruction.py
@@ -0,0 +1,37 @@
+"""
+Purpose: Instruction class
+Author: Hanich 10
+"""
+
+
+from pydantic import BaseModel, field_validator, ValidationError
+from typing import List
+
+
+COMMANDS = [
+    "CMD"
+]
+
+
+class Instruction(BaseModel):
+    """
+    A BaseModel class of instruction
+    Dockerfile is built from instructions
+    """
+    command: str
+    arguments: List[str]
+    
+    @field_validator('command')
+    @classmethod
+    def command_validate(self, value: str) -> str:
+        """
+        Checks if command is a valid field
+
+        :param value: A command
+        :raises ValidationError: When the command is not an available command
+        :return: The command 
+        """
+        if value not in COMMANDS:
+            raise ValidationError(f"{value} is not a valid command!")
+        
+        return value
