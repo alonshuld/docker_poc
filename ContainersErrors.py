af83140c05462a91116cf44be5adcacd2c5b6f0f
Doron Preger <predoron@gmail.com>
2025-06-23 18:53:49 +0300

    add new errors to ContainersErrors

--- ContainersErrors.py
+++ ContainersErrors.py
@@ -6,6 +6,9 @@ class ContainersErrors(Enum):
     TRYING_TO_STOP_A_RUNNING_CONTAINER = "Container is running right now! if you want to delete, stop it first."
     CPU_LIMIT_VALUE_OUT_OF_SCOPE = "CPU limit should be a precentage number between 0 and 100 (from the total CPU)"
     MEM_LIMIT_VALUE_OUT_OF_SCOPE = "RAM memory limit should be a positive number (scale units in MB)"
+    EXECUTABLE_COMMAND_NOT_FOUND = "Error: An executable command was not found."
+    INVALID_RUNNING_MODE = "trying to run container with invalid mode! must be interactive or detach."
+    START_UP_CONTAINER_ERROR = "A system or configuration error occurred during container start up: "
 
 class RunningContainerException(Exception):
     """ Cutsom Exception for wrong use with Running Containers """
