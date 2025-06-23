a58d0debd0d7676cff4c2b42ae2abfc0bbdfa9ce
Doron Preger <predoron@gmail.com>
2025-06-23 18:51:27 +0300

    implement the base of start function in container class

--- Container.py
+++ Container.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from uuid import UUID, uuid4
+import sys
+import subprocess
 from image import Image
 from ContainersErrors import ContainersErrors
 
@@ -29,15 +31,76 @@ class Container:
 
     def run(self) -> None:
         """ Run the container """
+        try:
+            # TO ADD: add cgroups for cpu and RAM control
+
+            unshare_command = self._build_unshare()
+
+            popen_kwargs = {}
+            # TO ADD: detach mode (add raise ValueError if mode is not valid)
+            
+            # interactive mode
+            popen_kwargs["stdin"] = sys.stdin
+            popen_kwargs["stdout"] = sys.stdout
+            popen_kwargs["stderr"] = sys.stderr
+
+            # run process on isolated enviroment
+            self.process = subprocess.Popen(unshare_command, **popen_kwargs)
+            # TO DO: assign the process to cgroup to limit CPU and RAM
+
+        except FileNotFoundError:
+            # TO ADD: clean up cgroup
+            raise FileNotFoundError(ContainersErrors.EXECUTABLE_COMMAND_NOT_FOUND)
+        except (PermissionError, OSError, subprocess.SubprocessError) as error:
+            # PermissionError: issues with file permissions (cgroup files)
+            # OSError: general OS errors (like invalid path for files)
+            # subprocess.SubprocessError: base for subprocess module errors (Popen cration might raise)
+            
+            if self.process and self.process.poll() is None:
+                self.process.terminate()
+
+            # TO ADD: clean up cgroup
+
+            raise error.add_note(ContainersErrors.START_UP_CONTAINER_ERROR)
+
         self.state = State.Running
-        # run container
-        raise NotImplementedError("Run Container will be implemented later")
+
 
     def stop(self) -> None:
         """ Stop the container running"""
         self.state = State.Stopped
         # stop container process
         raise NotImplementedError("Stop Container will be implemented later")
+    
+    def _build_unshare(self) -> list[str]:
+        """
+        function build the unshare command for creating new isolated enviroment
+        for the subprocess.Popen function
+        :return: list of str which contain the unshare command
+        """
+        # define the isolated enviroment
+        unshare_command = ["sudo", "unshare", 
+                           "--pid", # isolate process id
+                           "--mount", # isolate mount file sytem
+                           "--uts", # isolate hostname
+                           "--ipc", # isolate inter process comunication
+                           "--fork", # fork a new process to the new isolated enviroment
+                           "--chroot", f"/tmp/docker_poc/{str(self.id)}"] # isolate the file ststem (change root dir)
+        inner_commands = [
+            "mount -t proc proc /proc",
+            "mount -t sysfs sys /sys",
+            "mount --bind /dev /dev",
+            "mount -t tmpfs tmp /tmp",
+            f"hostname {self._name}"
+        ]
+
+        last_command_str = " ".join(["echo", "hello" ,"world"]) # change later to our commands
+        inner_commands.append(last_command_str)
+
+        full_shell_command = " && ".join(inner_commands)
+        unshare_command.extend(["/bin/sh", "-c", full_shell_command])
+
+        return unshare_command
 
 def is_percentage_number(number):
     """ helper function that check if number between 0 and 100
